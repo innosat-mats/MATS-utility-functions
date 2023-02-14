3d4c9aee7ed629a4af118a0a45a3a8dc5b23673b
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2023-02-14 17:23:06 +0100

    Added timeline schedules to the Utility function repo

--- src/mats_utils/rawdata/timeline_tools.py
+++ src/mats_utils/rawdata/timeline_tools.py
@@ -3,7 +3,7 @@ import datetime as DT
 import matplotlib.pyplot as plt
 
 
-def load_schedule(filename):
+def load_schedule(filename='data/timeline_schedule.db'):
     """Loads a timeline schedule overview and returns dataframe
 
     Arguments:
