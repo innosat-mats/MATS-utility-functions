07c2f559766421b4a6ec6a462844a85da914c82b
Björn Linder <bjorn.linder@misu.su.se>
2023-02-16 17:22:16 +0100

    Tidying

--- src/mats_utils/daily_preview/call.sh
+++ src/mats_utils/daily_preview/call.sh
@@ -18,24 +18,20 @@ echo 'Output to be saved in: '${outdir}
 # generate figures
 { python get_daily.py --version ${version} --outdir ${outdir} --level ${level}; } &
 pid=$!
-
-# wait while process runs
 wait $pid
 
 # generate animation
 { ffmpeg -stream_loop 0 -r 60 -i ${outdir}"/ALL/%d.png" -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ${outdir}${dates}.mp4; } &
 pid=$!
-
-# wait while process runs
 wait $pid
 
-# delete images in outdir/ALL
+# delete images in outdir/ALL/
 rm -r ${outdir}"/ALL/"
 
 # upload video
-python upload.py --file=${outdir}${dates}".mp4"
+{ python upload.py --file=${outdir}${dates}".mp4"
                  --title=${dates}
                  --description="Lorem"
                  --keywords="MATS"
                  --category="22"
-                 --privacyStatus="private"
+                 --privacyStatus="private"; }
