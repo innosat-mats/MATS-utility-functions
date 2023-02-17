ab7c77f18bfaf256365217ee780999e6080c2e7b
Björn Linder <bjorn.linder@misu.su.se>
2023-02-17 12:54:20 +0100

    Bug fixes in upload.py

--- src/mats_utils/daily_preview/call.sh
+++ src/mats_utils/daily_preview/call.sh
@@ -6,32 +6,41 @@ MATS_dir='/home/waves/projects/MATS/'
 # options 
 level="1a"
 version="0.4"
+snippet="True"
 
 # output
 dates=$(date +'%Y_%m_%d')
 outdir=${MATS_dir}'animations/daily/'$dates'/'
 
-echo -e 'Generating animation ...'
-echo 'L'${level}' data (v. '${version}')..'
-echo 'Output to be saved in: '${outdir}
+echo -e 'DAILY ANIMATION: Initiating ...'
+echo 'MATS DATA: L'${level}' data (v. '${version}')..'
+echo 'Generating figures in: '${outdir}
 
 # generate figures
-{ python get_daily.py --version ${version} --outdir ${outdir} --level ${level}; } &
+{ python get_daily.py --version ${version} --outdir ${outdir} --level ${level} --snippet ${snippet}; } &
 pid=$!
 wait $pid
+
+echo 'Generating animation .....'
 
 # generate animation
 { ffmpeg -stream_loop 0 -r 60 -i ${outdir}"/ALL/%d.png" -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ${outdir}${dates}.mp4; } &
 pid=$!
 wait $pid
 
+echo 'Removing figures .....'
+
 # delete images in outdir/ALL/
 rm -r ${outdir}"/ALL/"
+
+echo 'Uploading video .....'
 
 # upload video
 { python upload.py --file=${outdir}${dates}".mp4"
                  --title=${dates}
-                 --description="Lorem"
+                 --description="MATS test upload"
                  --keywords="MATS"
                  --category="22"
                  --privacyStatus="private"; }
+
+echo 'End of program .....'
