02bd906cec3e1f50db185a419d40a5ca2832904d
Björn Linder <bjorn.linder@misu.su.se>
2023-02-17 16:41:24 +0100

    Updated animation settings

--- src/mats_utils/daily_preview/call.sh
+++ src/mats_utils/daily_preview/call.sh
@@ -17,14 +17,14 @@ echo 'MATS DATA: L'${level}' data (v. '${version}')..'
 echo 'Generating figures in: '${outdir}
 
 # generate figures
-{ python get_daily.py --version ${version} --outdir ${outdir} --level ${level} --snippet ${snippet}; } &
+{ python get_daily.py --version ${version} --outdir ${outdir} --level ${level} --snippet; } &
 pid=$!
 wait $pid
 
 echo 'Generating animation .....'
 
 # generate animation
-{ ffmpeg -stream_loop 0 -r 60 -i ${outdir}"/ALL/%d.png" -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ${outdir}${dates}.mp4; } &
+{ ffmpeg -stream_loop 0 -r 60 -i ${outdir}"/ALL/%d.png" -vcodec libx264 -crf 0 ${outdir}${dates}.mp4; } &
 pid=$!
 wait $pid
 
@@ -37,10 +37,10 @@ echo 'Uploading video .....'
 
 # upload video
 { python upload.py --file=${outdir}${dates}".mp4"
-                 --title=${dates}
-                 --description="MATS test upload"
-                 --keywords="MATS"
-                 --category="22"
-                 --privacyStatus="private"; }
+                   --title=${dates}
+                   --description="MATS test upload"
+                   --keywords="MATS"
+                   --category="22"
+                   --privacyStatus="private"; }
 
 echo 'End of program .....'
