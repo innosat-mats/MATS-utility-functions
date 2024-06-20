df525d0f0c97d51b59e0f0e002a090d4669cb7ec
Lukas Krasauskas <lukas.krasauskas@misu.su.se>
2024-06-20 09:51:47 +0200

    Common grid related functions

--- src/mats_utils/geolocation/coordinates.py
+++ src/mats_utils/geolocation/coordinates.py
@@ -3,10 +3,12 @@ from skyfield.positionlib import Geocentric, ICRF
 from skyfield.units import Distance
 from numpy.linalg import norm
 from scipy.optimize import minimize_scalar
+from scipy.interpolate import RegularGridInterpolator
 from skyfield.api import wgs84
 from skyfield.api import load
 from skyfield.units import Distance
 from scipy.spatial.transform import Rotation as R
+from scipy.spatial.transform import Slerp
 from scipy.interpolate import CubicSpline
 from mats_l1_processing.pointing import pix_deg
 import numpy as np
@@ -20,6 +22,214 @@ from pandas import DataFrame, Timestamp  # type: ignore
 from skyfield import api as sfapi
 from scipy.interpolate import RegularGridInterpolator
 import math
+
+
+def get_deg_map(image):
+    """
+    Function for calculating pixel positions in terms
+    of euler angles within the frame of the specific channel
+
+    Arguments:
+        image (pandas row or dict)
+
+    Returns:
+        ndarray, the two Euler angles as 2D grids, stacked in third dim.
+    """
+
+    dmap = np.zeros((image["NCOL"] + 1, image["NROW"], 2))
+    for i in range(dmap.shape[0]):
+        for j in range(dmap.shape[1]):
+            dmap[i, j, :] = pix_deg(image, i, j)
+    return dmap
+
+
+def make_ref_grid(df, sample_factor=0.5, ext_factor=1.1):
+    """
+    This calculates an (example of) common grid for MATS images in terms
+    of euler angles in satellite body frame. Can be stored/edited before
+    actually interpolating to it.
+
+    Arguments:
+        df - Pandas data frame with at least one IR1 image;
+        sample_factor - the grid constant for the common grid will be
+              IR1 grid constant * sample_factor. Default: 0.5
+        ext_factor - defines the size of margins around image, needed to
+              accomodate the other channels. Ref. grid size = IR1 size * ext_factor
+    Returns:
+        deg_map - ndarray, the two Euler angles as 2D grids, stacked in third dim.
+        grids - tuple, the two Euler angles as 1-D arrays
+    """
+
+    # Get IR1 grid as a prototype
+    ir1_images = df[df['CCDSEL'] == 1]
+    assert len(ir1_images) > 0, "There are no IR1 images in the selected dataset, choose a longer one."
+    IR1_qprime = R.from_quat(ir1_images.iloc[0]["qprime"])
+    IR1_deg_map = get_deg_map(ir1_images.iloc[0])
+    conv_fix, _ = R.align_vectors([[1, 0, 0], [0, 1, 0]], [[0, 0, -1], [0, -1, 0]])
+    xgrid_IR1, ygrid_IR1 = IR1_deg_map[:, 0, 0], IR1_deg_map[0, :, 1]
+
+    # Find the positions of IR1 grid corners in body frame
+    corner_ang = np.zeros((2, 2, 2))
+    for i in [0, -1]:
+        for j in [0, -1]:
+            r = conv_fix * IR1_qprime * R.from_euler('xyz', [0, IR1_deg_map[i, j, 1],
+                                                     IR1_deg_map[i, j, 0]], degrees=True)
+            _, corner_ang[i, j, 1], corner_ang[i, j, 0] = r.as_euler('xyz', degrees=True)
+
+    # Construct xgrid and ygrid
+    grids = []
+    for c, grid1 in enumerate([xgrid_IR1, ygrid_IR1]):
+        step = sample_factor * np.diff(grid1).mean()
+        extent = np.abs(corner_ang[:, :, c].min() - corner_ang[:, :, c].max())
+        start = corner_ang[:, :, c].min() - (ext_factor - 1) * extent / 2
+        extent *= ext_factor
+        gstart, gstop = [np.floor(x / step) * step for x in [start, start + extent]]
+        grids.append(np.arange(gstart, gstop + step / 2, step))
+    yy, xx = np.meshgrid(grids[1], grids[0])
+    deg_map = np.zeros((xx.shape[0], xx.shape[1], 2))
+    deg_map[:, :, 0] = xx
+    deg_map[:, :, 1] = yy
+    return deg_map, grids
+
+
+def to_ref(df, chn, ref_map, time_ref_chn=None):
+    """
+    Reinterpolates MATS images onto reference grid.
+
+    Arguments:
+        df - Pandas data frame;
+        chn - string, name of the channel to reinterpolate, e.g. "IR1".
+        ref_map - ndarray, 2-D Euler angle reference grid. Shape is (num. cols, num. rows, 2).
+        time_ref_chn - bool, not implemented yet.
+    Returns:
+        ndarray, images on reference grid.
+    """
+
+    # Select images for the specified channel
+    CCDSEL = {"IR1": 1, "IR2": 4, "IR3": 3, "IR4": 2, "UV1": 5, "UV2": 6}
+    sel = df[df['CCDSEL'] == CCDSEL[chn]]
+    numimg = len(sel)
+    assert numimg > 0
+
+    # Map ref. grid onto channel
+    ref_angs = np.zeros_like(ref_map)
+    chn_qprime = R.from_quat(sel.iloc[0]["qprime"])
+    chn_deg_map = get_deg_map(sel.iloc[0])
+    chn_grids = (chn_deg_map[:, 0, 0], chn_deg_map[0, :, 1])
+    conv_fix, _ = R.align_vectors([[1, 0, 0], [0, 1, 0]], [[0, 0, -1], [0, -1, 0]])
+    fix_rot = R.inv(conv_fix * chn_qprime)
+
+    for i in range(ref_map.shape[0]):
+        for j in range(ref_map.shape[1]):
+            r = fix_rot * R.from_euler('xyz', [0, ref_map[i, j, 1], ref_map[i, j, 0]], degrees=True)
+            _, ref_angs[i, j, 1], ref_angs[i, j, 0] = r.as_euler('xyz', degrees=True)
+            # pix_vec = r.apply([1, 0, 0])
+            # ref_angs[i, j, :] = [np.rad2deg(np.arcsin(pix_vec[i])) for i in [1, 2]]
+
+    # Interpolate
+    res = np.zeros((numimg, ref_map.shape[0], ref_map.shape[1]))
+    for k in range(numimg):
+        img = sel.iloc[k]["ImageCalibrated"].T
+        interp = RegularGridInterpolator(chn_grids, img, method='cubic', bounds_error=False, fill_value=np.nan)
+        res[k, :, :] = interp((ref_angs[:, :, 0], ref_angs[:, :, 1]))
+    return res
+
+
+def calc_nadir_orbplane_angles(df, chn, ref_map):
+    """
+    Calculates angle to nadir and angle to orbital plane (defined as perpenticular to r x v).
+
+    Arguments:
+        df - Pandas data frame with MATS data;
+        chn - string, name of the channel (e.g. "IR1") that determines the times for which angles are calculated;
+        ref_map - ndarray, 2-D Euler angle reference grid. Shape is (num. cols, num. rows, 2).
+    Returns:
+        nadir_angle, orbplane_angle - 2D darrays, respective angles for the reference grid.
+    """
+
+    # Select images for the specified channel
+    CCDSEL = {"IR1": 1, "IR2": 4, "IR3": 3, "IR4": 2, "UV1": 5, "UV2": 6}
+    sel = df[df['CCDSEL'] == CCDSEL[chn]]
+    numimg = len(sel)
+
+    # Calculate angles
+    nadir_angle = np.zeros((numimg, ref_map.shape[0], ref_map.shape[1]))
+    orbplane_angle = nadir_angle.copy()
+    conv_fix, _ = R.align_vectors([[1, 0, 0], [0, 1, 0]], [[0, 0, -1], [0, -1, 0]])
+
+    for k in range(numimg):
+        align_rot = to_ref_att(sel.iloc[k]["afsGnssStateJ2000"])
+        rot = align_rot * R.from_quat(np.roll(sel.iloc[k]["afsAttitudeState"], -1)) * conv_fix.inv()
+        for i in range(ref_map.shape[0]):
+            for j in range(ref_map.shape[1]):
+                pix_vec = (rot * R.from_euler('xyz', [0, ref_map[i, j, 1],
+                                              ref_map[i, j, 0]], degrees=True)).apply([1, 0, 0])
+                nadir_angle[k, i, j] = np.rad2deg(np.arccos(-pix_vec[2]))
+                orbplane_angle[k, i, j] = np.rad2deg(np.arccos(pix_vec[1])) - 90
+    return nadir_angle, orbplane_angle
+
+
+def to_ref_att(gnss):
+    """
+    Calculates the roatation needed to go from body frame to a frame where: z axis is towards zenith,\
+    sat. velocity is aligned with x axis and angular momentum (r x v) is aligned with y axis.
+
+    Arguments:
+        gnss - an array of 6 components, GNSS-based statelite position ("afsGnssStateJ2000").
+    Returns:
+        scipy rotation
+    """
+
+    rad_vec = gnss[:3] / np.linalg.norm(gnss[:3])
+    norm_vec = np.cross(rad_vec, gnss[3:])
+    norm_vec /= np.linalg.norm(norm_vec)
+    res, rssd = R.align_vectors([[0, 1, 0], [0, 0, 1]], [norm_vec, rad_vec])
+    return res
+
+
+def get_temporal_rotation(df, data_chn, ref_chn):
+    """
+    Calculates rotation corrections needed to compensate for temporal mismatch between channels.
+
+
+    Arguments:
+        df - Pandas data frame with MATS data;
+        data_chn - corrections will be calculated for data of this channel;
+        ref_chn - corrections will be calculated for interpolating data_chn data onto ref_chn times.
+    Returns:
+        trots - list of scipyrotations;
+        times = list of datetime objects representimng times for which corrections are calculated.
+    """
+
+    assert data_chn != ref_chn
+
+    # Get timestamps and sat. pointing for data data channel and ref. channel images within selection
+    CCDSEL = {"IR1": 1, "IR2": 4, "IR3": 3, "IR4": 2, "UV1": 5, "UV2": 6}
+    sat_att, data_time, ref_time = [df[df['CCDSEL'] == CCDSEL[chn]][var] for chn, var in
+                                    [(data_chn, "afsAttitudeState"), (data_chn, "EXPDate"), (ref_chn, "EXPDate")]]
+    assert len(ref_time) > 2
+    assert len(data_time) > 2
+    ref_time_og = ref_time
+    ref_tp = ref_time[0]
+    data_time, ref_time = [(arr - ref_tp).total_seconds() for arr in [data_time, ref_time]]
+
+    # Check that both channels have the same temporal cadence
+    data_step, ref_step = [np.diff(arr).mean() for arr in [data_time, ref_time]]
+    assert np.abs(data_step / ref_step - 1) < 0.01
+
+    # Initialize rotation interpolator
+    rots = Slerp(data_time, [R.from_quat(q) for q in sat_att])
+
+    # Assign each image to appropriate reference time and interpolate
+    idxs = [np.argmin(np.abs(d - ref_time)) for d in data_time]
+    trots, times = [], []
+    for i, idx in enumerate(idxs):
+        out = (data_time[i] < ref_time[0]) or (data_time[i] > ref_time[-1])
+        misaligned = np.abs((data_time[i] - ref_time[idx]) / ref_step) > 0.5
+        if not (out or misaligned):
+            trots.append(R.from_quat(sat_att[i]).inv() * rots(ref_time[idx]))
+            times.append(ref_time_og[idx])
+    return trots, times
 
 
 def funheight(s, t, pos, FOV):
@@ -206,7 +416,7 @@ def angles(ccditem):
     sundir= TP_pos.at(t).observe(sun).apparent()
     obs= sundir.altaz()
     TPsza = (90-obs[0].degrees)
-    TPssa = (np.rad2deg(np.arccos(np.dot(FOV,sundir.position.m/norm(sundir.position.m)))))
+    TPssa = (np.rad2deg(np.arccos(np.dot(FOV,sundir.position.m / norm(sundir.position.m)))))
     return nadir_sza, TPsza, TPssa, tpLT
 
 
